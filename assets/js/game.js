193470d4fcb80bbc60b8d8e8222b361427f55537
delaney <delaneycharney@gmail.com>
2020-11-01 16:48:41 -0500

    local highscore saved

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,9 +14,9 @@ var playerInfo = {
     attack: 10,
     money: 10,
     reset: function() {
-        this.health;
-        this.money;
-        this.attack;
+        this.health = 100;
+        this.money = 10;
+        this.attack = 10;
     },
     refillHealth: function() {
         if (this.money >= 7) {
@@ -164,7 +164,7 @@ var startGame = function() {
             pickedEnemyObj.health = randomNumber(40, 60);
 
             // use debugger to pause script from running and check what's going on at that moment in the code
-            debugger;
+            //debugger;
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyObj);
@@ -187,22 +187,34 @@ var startGame = function() {
 };
 
 var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-      window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    debugger;
+  
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player have more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+  
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } 
     else {
-      window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+  
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
-
+  
     if (playAgainConfirm) {
-    // restart the game
-    startGame();
+        startGame();
     } 
     else {
-    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
 
